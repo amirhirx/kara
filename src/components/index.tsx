9d6f074718dd642976e7a5bea932480ac17b681a
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-03-29 21:28:42 +0330

    Create TaskForm component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,8 @@
-import Menu from "./menu/Menu"
-import Task from "./task/Task"
-import TasksContainer from "./tasksContainer/TasksContainer"
-import AddTaskDialog from "./addTaskDialog/AddTaskDialog"
-import EditTaskDialog from "./editTaskDialog/EditTaskDialog"
+import Menu from "./menu/Menu";
+import Task from "./task/Task";
+import TasksContainer from "./tasksContainer/TasksContainer";
+import AddTaskDialog from "./addTaskDialog/AddTaskDialog";
+import EditTaskDialog from "./editTaskDialog/EditTaskDialog";
+import TaskForm from "./taskForm/TaskForm";
 
-export { Menu, Task, TasksContainer, AddTaskDialog, EditTaskDialog }
+export { Menu, Task, TasksContainer, AddTaskDialog, EditTaskDialog, TaskForm };
---
+++ src/components/taskForm/TaskForm.tsx
@@ -0,0 +1,66 @@
+import type { ITask } from "../../types/task";
+
+export default function TaskForm({
+    title,
+    task,
+    updateTask,
+    clickLabel,
+    onClick,
+}: {
+    title: string;
+    task: ITask;
+    updateTask: (newTask: ITask) => void;
+    clickLabel: string;
+    onClick: () => void;
+}) {
+    return (
+        <div className="w-11/12 md:w-8/12 mx-auto pt-8 text-[var(--text-color)] space-y-2">
+            <h2 className="text-2xl font-black">{title}</h2>
+            <div className="flex flex-col">
+                <label
+                    htmlFor="task-title"
+                    className="text-lg font-bold mb-1"
+                >
+                    عنوان
+                </label>
+                <input
+                    type="text"
+                    name="task-description"
+                    id="task-description"
+                    value={task.title}
+                    onChange={(e) =>
+                        updateTask({ ...task, title: e.target.value })
+                    }
+                    className="p-2 rounded-lg text-xl border bg-[var(--primary-color)] border-[var(--secondary-color)] outline-none shadow"
+                />
+            </div>
+            <div className="flex flex-col">
+                <label
+                    htmlFor="task-text"
+                    className="text-lg font-bold mb-1"
+                >
+                    توضیحات
+                </label>
+                <textarea
+                    name="task-text"
+                    id="task-text"
+                    cols={30}
+                    rows={10}
+                    value={task.text}
+                    onChange={(e) =>
+                        updateTask({ ...task, text: e.target.value })
+                    }
+                    className="p-2 min-h-14 h-40 max-h-80 rounded-lg border bg-[var(--primary-color)] border-[var(--secondary-color)] outline-none shadow"
+                />
+            </div>
+            <div className="py-2">
+                <button
+                    className="py-2 px-4 rounded-lg font-bold bg-green-500 text-white cursor-pointer transition active:bg-green-600 active:scale-95"
+                    onClick={onClick}
+                >
+                    {clickLabel}
+                </button>
+            </div>
+        </div>
+    );
+}
