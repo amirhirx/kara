69d72e16f4799e3123ed988f82baeac43945ac67
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-03-29 21:28:59 +0330

    Create New page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { Menu } from "./components";
+import New from "./pages/New";
 
 function App() {
     return (
@@ -10,6 +11,10 @@ function App() {
                 <Route
                     path="/"
                     element={<Home />}
+                />
+                <Route
+                    path="/new"
+                    element={<New />}
                 />
             </Routes>
         </main>
