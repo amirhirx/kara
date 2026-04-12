cbb898a8527b126526a54e2fc8cfd304348561a4
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-04-12 20:53:11 +0330

    add projects & new project button to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,45 @@
-import { useContext } from "react";
-import { TasksContainer } from "../components";
-import { TodoContext } from "../contexts/todoContext";
+import { useDispatch, useSelector } from "react-redux";
+import { Button, ProjectCard, ProjectCardContainer } from "../components";
+import type { RooteState } from "../contexts/store";
+import { newProject } from "../contexts/projectsSlice";
 
 export default function Home() {
-    const { todo } = useContext(TodoContext);
+    const dispatch = useDispatch();
+    const projects = useSelector(
+        (state: RooteState) => state.projects.projects,
+    );
+
+    const createNewProject = () => {
+        const title = prompt("Enter project title:") || "";
+        const description = prompt("Enter project description:") || "";
+        if (title?.length >= 3) {
+            dispatch(
+                newProject({
+                    title: title,
+                    description: description,
+                }),
+            );
+        } else {
+            alert("Your title must be more 3 characters");
+        }
+    };
     return (
-        <div className="pt-4">
-            <TasksContainer tasks={todo} />
+        <div className="space-y-2 pt-2">
+            <div className="w-11/12 mx-auto px-2">
+                <Button onClick={createNewProject}>پروژه جدید</Button>
+            </div>
+            <ProjectCardContainer>
+                {projects.map(({ id, title, description }) => {
+                    return (
+                        <ProjectCard
+                            key={id}
+                            id={id}
+                            title={title}
+                            description={description}
+                        />
+                    );
+                })}
+            </ProjectCardContainer>
         </div>
     );
 }
