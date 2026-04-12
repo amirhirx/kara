c625a98b5814b23431b6491718e518a68438c8b4
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-04-12 20:56:26 +0330

    Wrap ProjectCard to Link

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function ProjectCard({
+    id,
     title,
     description,
 }: {
+    id: string;
     title: string;
     description: string;
 }) {
     return (
-        <div className="w-full md:w-74 bg-[var(--primary-color)] text-[var(--text-color)] rounded-lg">
+        <Link
+            to={`/project/${id}`}
+            className="w-full md:w-74 bg-[var(--primary-color)] text-[var(--text-color)] rounded-lg"
+        >
             <div className="h-18 py-2 px-4 bg-[var(--secondary-color)]/35 relative">
                 <div className="w-18 h-18 rounded-2xl bg-[var(--secondary-color)] absolute -bottom-5"></div>
             </div>
@@ -14,6 +21,6 @@ export default function ProjectCard({
                 <h2 className="font-bold text-lg line-clamp-1">{title}</h2>
                 <p className="text-sm font-light line-clamp-3">{description}</p>
             </div>
-        </div>
+        </Link>
     );
 }
