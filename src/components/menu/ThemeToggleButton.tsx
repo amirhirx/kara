38204c14d0ae9c69649b10b07682f385704b313f
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-04-13 18:40:56 +0330

    Replace Heroicons with Lucide icons

--- src/components/menu/ThemeToggleButton.tsx
+++ src/components/menu/ThemeToggleButton.tsx
@@ -1,20 +1,23 @@
 import { useDispatch, useSelector } from "react-redux";
-import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { Moon, Sun } from "lucide-react";
 import type { RooteState } from "../../contexts/store";
 import { toggleTheme } from "../../contexts/themeSlice";
+import { ThemeMode } from "../../types";
 
 export default function ThemeToggleButton() {
     const dispatch = useDispatch();
-    const theme = useSelector((state: RooteState) => state.theme.theme);
+    const theme: ThemeMode = useSelector(
+        (state: RooteState) => state.theme.theme,
+    );
     return (
         <div
             onClick={() => dispatch(toggleTheme())}
             className="transition active:scale-95"
         >
-            {theme ? (
-                <MoonIcon className="h-6 w-6 text-[var(--text-color)]" />
+            {theme === ThemeMode.dark ? (
+                <Moon className="h-6 w-6 text-[var(--text-color)]" />
             ) : (
-                <SunIcon className="h-6 w-6 text-[var(--text-color)]" />
+                <Sun className="h-6 w-6 text-[var(--text-color)]" />
             )}
         </div>
     );
--- src/components/newBoardButton/NewBoardButton.tsx
+++ src/components/newBoardButton/NewBoardButton.tsx
@@ -1,4 +1,4 @@
-import { PlusIcon } from "@heroicons/react/16/solid";
+import { Plus } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { newBoard } from "../../contexts/projectsSlice";
 
@@ -25,7 +25,7 @@ export default function NewBoardButton({ projectId }: { projectId: string }) {
             className="w-60 bg-[var(--primary-color)] text-[var(--text-color)] py-2.5 px-4 rounded-lg text-sm font-semibold flex gap-1 justify-center items-center opacity-75 hover:opacity-100 transition active:scale-95"
         >
             افزودن لیست جدید
-            <PlusIcon className="w-5" />
+            <Plus className="w-5" />
         </button>
     );
 }
