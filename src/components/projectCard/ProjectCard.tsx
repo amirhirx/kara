b74c31501d3198817dc9f54e6ec33964fc89d7e1
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-04-18 21:09:08 +0330

    Update ProductCard styles

--- src/components/projectCard/ProjectCard.tsx
+++ src/components/projectCard/ProjectCard.tsx
@@ -14,18 +14,19 @@ export default function ProjectCard({
     return (
         <Link
             to={`/project/${id}`}
-            className="w-full md:w-74 bg-primary text-text rounded-lg"
+            className="w-full md:w-74 bg-primary text-text rounded-lg shadow-lg"
+            draggable
         >
-            <div className="h-18 py-2 px-4 bg-secondary/35 relative">
-                <div className="w-18 h-18 rounded-2xl bg-secondary absolute -bottom-5 p-1 flex items-center justify-center">
+            <div className="h-18 py-12 px-4 bg-secondary/35 relative">
+                <div className="w-18 h-18 rounded-2xl bg-secondary absolute -bottom-5 p-1 flex items-center justify-center shadow-xl">
                     <span className="text-5xl font-black select-none">
                         {emojie}
                     </span>
                 </div>
             </div>
-            <div className="pt-6 pb-2 px-4">
-                <h2 className="font-bold text-lg line-clamp-1">{title}</h2>
-                <p className="text-sm font-light line-clamp-3">{description}</p>
+            <div className="pt-6 pb-4 px-4 h-28">
+                <h2 className="font-bold text-xl line-clamp-1">{title}</h2>
+                <p className="text-sm line-clamp-2">{description}</p>
             </div>
         </Link>
     );
