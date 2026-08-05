1e4c5ef88d8c39ffac62db11161d5414453de905
AmirHossein Hosseinian <amirhirx@gmail.com>
2026-08-05 21:44:58 +0330

    chore: move front-end to frontend directory

--- frontend/src/components/menu/Menu.tsx
+++ frontend/src/components/menu/Menu.tsx
@@ -5,7 +5,7 @@ function Menu() {
     // TODO: show new task dialog with shorcuts
 
     return (
-        <nav className="bg-primary py-2 px-8 flex items-center justify-between shadow border border-secondary">
+        <nav className="w-8 h-[96vh] my-auto rounded-2xl fixed top-3 right-5 bg-primary py-4 px-8 flex flex-col items-center justify-between shadow border border-secondary">
             <Link
                 to={"/"}
                 className="text-xl font-black text-text select-none"
